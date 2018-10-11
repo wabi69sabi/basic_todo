4ce3173f5e8b45b7cb5915ade6c34d4f12c79626
wabi69sabi <wabisabisf@gmail.com>
2018-10-11 12:00:32 +0200

    added a body to the task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181010122219) do
+ActiveRecord::Schema.define(version: 20181011095418) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "body"
   end
 
 end
